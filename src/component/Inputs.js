f1ecb9d9a0e39149dfa179ec47fa437b7bf3f0bf
Shreyascr5 <shreyas050801@gmail.com>
2024-02-02 15:03:56 +0530

    implemented ui

--- src/component/Inputs.js
+++ src/component/Inputs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { UilSearch,UilLocationPoint } from '@iconscout/react-unicons';
+import { UilSearch,UilLocationPoint,UilCelsius,UilFahrenheit } from '@iconscout/react-unicons';
 
 function Inputs() {
   return (
@@ -11,6 +11,10 @@ function Inputs() {
             <UilSearch size={25} className="text-white cursor-pointer  transition ease-out hover:scale-125"/>
             <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
         </div>
+        <div className='flex flex-row w-1/4 items-center justify-center '>
+         <button name='metric' className='text-xl text-white font-light'><UilCelsius/></button>
+        <p className='text-xl text-white mx-1'>|</p>
+        <button name='imperial'className='text-xl text-white font-light'><UilFahrenheit/></button></div>
     </div>
   )
 }
---
+++ src/component/TimeandLocation.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeandLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 1 Feb 2024 | Local time: 05:30 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Bangalore,Blr</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeandLocation;
