0d631a7cfdd0a4620861d6834f2bbc58fffccb59
Shreyascr5 <shreyas050801@gmail.com>
2024-02-02 14:37:05 +0530

    implemented search tab ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import './index.css';
+import Inputs from './component/Inputs';
+import TopButton from './component/TopButton';
+
 export default function App() {
   return (
-    <div className='text-2xl font-bold text-red-500'>App</div>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
+   <TopButton/>
+   <Inputs/>
+    </div>
   )
 }
